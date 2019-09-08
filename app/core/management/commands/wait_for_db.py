fc19623ba366c39ed331d197c0c17bd93263d337
Michael Hixson <hixsonweb@gmail.com>
2019-09-08 16:41:51 -0500

    trying to all all files

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,20 @@
+import time
+
+from django.db import connections
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to pause execution until db is availabel"""
+
+    def handle(self, *args, **options):
+        self.stdout.write('Waiting for the Database...')
+        db_conn = None
+        while not db_conn:
+            try:
+                db_conn = connections['default']
+            except OperationalError:
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+        self.stderr.write(self.style.SUCCESS('Database is available.'))
